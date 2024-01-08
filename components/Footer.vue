a83bb4c734ca7b29a7e15f4c174b33b85ad60c04
sammytrindade <samaratrindaderibeiro@gmail.com>
2024-01-08 04:59:13 -0300

    :sparkles: feat: Footer  project

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,11 @@
+<!-- Rodapé da página
+  elemento pai: fotter (Estilização do backgroud-color e flex box)
+  elemento filho: central-test (Container de estilização que contém a tag HTML "p" dentro de footer)
+  elemento filho: footer-content (Container que contém os icons, usado para posicioná-los ao mesmo tempo)
+  elemento neto: social-icons (Filho de footer-content, para estilização dos icons)
+  elemento filho: designer-text (Container usado para posicionar a tag HTML "p")
+-->
+
 <template>
   <footer class="footer">
     <div class="central-text">
@@ -11,21 +19,34 @@
         <a
           href="mailto:samaratrindaderibeiro@hotmail.com?subject=&body="
           title="Email"
-          ><EmailIcon
-        /></a>
-        <a href="https://github.com/sammytrindade" title="Github"
-          ><GithubIcon
-        /></a>
-        <a href="https://www.linkedin.com/in/samaratrindade/" title="LinkedIn"
-          ><LinkedinIcon
-        /></a>
-        <a href="https://twitter.com/SammyTrindade" title="Twitter"
-          ><TwitterIcon
-        /></a>
+          target="_blank"
+          ><img src="../assets/Email.png" alt="Email image" />
+        </a>
+        <a
+          href="https://github.com/sammytrindade"
+          title="Github"
+          target="_blank"
+          ><img src="../assets/Github.png" alt="Logo github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/samaratrindade/"
+          title="LinkedIn"
+          target="_blank"
+          ><img src="../assets/Linkedin.png" alt="Logo linkedin" />
+        </a>
+
+        <a
+          href="https://twitter.com/SammyTrindade"
+          title="Twitter"
+          target="_blank"
+          ><img src="../assets/Twitter.png" alt="Logo twitter" />
+        </a>
+
         <a
           href="https://api.whatsapp.com/send?phone=5521993601558"
           title="WhatsApp"
-          ><WhatsappIcon
+          target="_blank"
+          ><img src="../assets/Whatsapp.png" alt="Phone Image"
         /></a>
       </div>
       <div class="designer-text">
@@ -36,17 +57,17 @@
 </template>
 
 <script setup lang="ts">
-import EmailIcon from "~/components/icons/email.vue";
-import GithubIcon from "~/components/icons/github.vue";
-import LinkedinIcon from "~/components/icons/linkedin.vue";
-import TwitterIcon from "~/components/icons/twitter.vue";
-import WhatsappIcon from "~/components/icons/whatsapp.vue";
+// import EmailIcon from "~/components/icons/email.vue";
+// import GithubIcon from "~/components/icons/github.vue";
+// import LinkedinIcon from "~/components/icons/linkedin.vue";
+// import TwitterIcon from "~/components/icons/twitter.vue";
+// import WhatsappIcon from "~/components/icons/whatsapp.vue";
 </script>
 
 <style scoped>
 .footer {
-  background-color: #F68D2E;
-  color: #22224F;
+  background-color: #f68d2e;
+  color: #22224f;
   text-align: center;
   padding: 20px;
 }
@@ -61,15 +82,111 @@ import WhatsappIcon from "~/components/icons/whatsapp.vue";
 .central-text {
   font-family: "Audiowide", cursive;
   flex-grow: 1;
+  flex-wrap: wrap;
+  width: 100%;
+  text-align: start;
+}
+
+.central-text p {
+  font-size: 47px;
+  padding: 0 3em;
 }
 
 .social-icons {
   margin-top: 10px;
+  margin-left: 10em;
   display: flex;
   gap: 10px;
+}
+.social-icons img {
+  width: 43px;
 }
 
 .designer-text {
   margin-top: 10px;
+}
+.designer-text p {
+  font-size: 32px;
+}
+
+@media (max-width: 1440px) {
+  .central-text p {
+    font-size: 33px;
+    padding: 0 2em;
+  }
+
+  .social-icons {
+    margin-top: 10px;
+    margin-left: 6em;
+    display: flex;
+    gap: 10px;
+  }
+
+  .designer-text p {
+    font-size: 22px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .central-text p {
+    font-size: 23px;
+    padding: 0 2em;
+  }
+
+  .social-icons {
+    margin-top: 10px;
+    margin-left: 3em;
+    display: flex;
+    gap: 10px;
+  }
+  .designer-text p {
+    font-size: 19px;
+  }
+  .social-icons img {
+    width: 33px;
+  }
+}
+
+@media (max-width: 768px) {
+  .central-text p {
+    font-size: 16px;
+    padding: 0 2em;
+  }
+
+  .social-icons {
+    margin-top: 10px;
+    margin-left: 2em;
+    display: flex;
+    gap: 10px;
+  }
+
+  .designer-text p {
+    font-size: 14px;
+  }
+
+  .social-icons img {
+    width: 23px;
+  }
+}
+@media (max-width: 768px) {
+  .central-text p {
+    font-size: 12px;
+    padding: 0 2em;
+  }
+
+  .social-icons {
+    margin-top: 10px;
+    margin-left: 1.5em;
+    display: flex;
+    gap: 10px;
+  }
+
+  .designer-text p {
+    font-size: 10px;
+  }
+
+  .social-icons img {
+    width: 18px;
+  }
 }
 </style>
