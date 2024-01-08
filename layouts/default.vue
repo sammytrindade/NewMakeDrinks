5181f449984602644c06d4e0154795c7bb683dd5
sammytrindade <samaratrindaderibeiro@gmail.com>
2024-01-08 05:03:03 -0300

    :bug: chore: Add default layout, but this is not work

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
+<!-- Erro ao usar o layout, para deixar header e footer como padrão
+    diversas pessoas com o mesmo erro. Para mais detalhes:
+    https://github.com/nuxt/nuxt/issues/24912 -->
+
 <template>
-    <div>
-        Layout Padrão
-        <slot>
-        </slot>
-        Rodapé padrão
-    </div>
+  <div>
+    <!-- <Header />
+        <slot></slot>
+        <Footer /> -->
+  </div>
 </template>
